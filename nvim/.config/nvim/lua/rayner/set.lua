d20a5096ef6fffe43d2735c3b38b5040f1a8d3d6
Rayner Sulyak <raynersulyak@gmail.com>
2022-09-22 23:21:07 -0300

    first set

---
+++ nvim/.config/nvim/lua/rayner/set.lua
@@ -0,0 +1,11 @@
+
+-- tab sanity
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
